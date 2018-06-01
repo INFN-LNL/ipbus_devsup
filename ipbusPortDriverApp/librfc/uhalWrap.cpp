046f94e6fa347baddac40ad8673123ad9ad246ce
Stefano Pavinato <stefano.pavinato@lnl.infn.it>
2018-06-01 19:35:43 +0200

    deleted unuseful classes

--- ipbusPortDriverApp/librfc/uhalWrap.cpp
+++ ipbusPortDriverApp/librfc/uhalWrap.cpp
@@ -1,34 +1,14 @@
 #include "uhalWrap.h"
 
-conManWr::conManWr(char *con) {
-	_con = new ConnectionManager(con);
-}
 
 
-//int hwIntWr::connect(){
-//	registersIPBUS r(*this, "d");
-//	uint32_t n = r.get(0);
-//	if (n == CTRL_WORLD)
-//	    return 0;
-//	else return 1;
-//}
 
 extern "C" {
 
-conManWr * newconManWr(void *c){
-	return new conManWr((char*)c);
-}
 
 hwIntWr *newhwIntWr(void *c, void *ch){
 	return new hwIntWr((const char *) c, (const char *) ch);
 }
 
-//hwIntWr *newhwIntWr(void *c, void *ch){
-//	return new hwIntWr((ConnectionManager *) c, (const char *) ch);
-//}
-
-//ConnectionManager* getConn(myUhal * m){
-//	return m->getConn();
-//}
 
 };
