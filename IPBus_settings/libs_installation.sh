5a2b1b992f5d75cbe0ee78eedc362b7732262655
Davide Marcato <davide.marcato@lnl.infn.it>
2018-06-14 17:04:07 +0200

    Update libs_installation.sh

--- IPBus_settings/libs_installation.sh
+++ IPBus_settings/libs_installation.sh
@@ -6,23 +6,9 @@ su
 cd /opt
 mkdir cactus
 cd cactus
+sudo yum -y install epel-release
 sudo yum -y install make rpm-build git-core erlang gcc-c++ boost-devel pugixml-devel python-devel
 git clone --depth=1 -b v2.6.1  https://github.com/ipbus/ipbus-software.git
 cd ipbus-software
 make
-make rpm
 sudo make install
-
-# Install boost
-cd /extern/boost
-tar --bzip2 -xf boost_1_53_0.tar.bz2
-cd boost_1_53_0
-./bootstrap.sh --prefix=/opt/cactus
-./b2 install
-cd ../../../include
-
-# Install pugixml
-git clone https://github.com/zeux/pugixml
-cd pugixml
-git checkout v1.9
-exit
