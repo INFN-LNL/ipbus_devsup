7471e47c7e7df6e87edd7aca96d8dd775118acb3
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2019-09-13 14:41:41 +0200

    Added passages to install ipbus libraries on ubuntu18

---
+++ IPBus_settings/libs_install_ubuntu18.sh
@@ -0,0 +1,9 @@
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cactus/lib" >> ~/.bashrc
+echo "export PATH=/opt/cactus/bin:$PATH" >> ~/.bashrc
+apt install pugixml-dev erlang
+sudo mkdir /opt/cactus
+cd /opt/cactus
+sudo git clone --depth=1 -b v2.6.4 https://github.com/ipbus/ipbus-software.git
+cd ipbus-software
+sudo make
+sudo make install
